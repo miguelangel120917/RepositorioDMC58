3e9beb02d44fdbf9a35f483359a9cf3d616f6085
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-12 19:56:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ st.write("Elaborado por: Miguel Limaquispe")
 
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
 
-if session == "Sesión 1":
+if sesion == "Sesión 1":
   st.write("Bienvenido a la sesión 1")
-elif session =="Sesión 2":
+elif sesion =="Sesión 2":
   st.write("Bienvenido a la sesión 2")
-elif session =="Sesión 3":
+elif sesion =="Sesión 3":
   st.write("Bienvenido a la sesión 3")
 else:
   st.write("Bienvenido a la sesión 4")
