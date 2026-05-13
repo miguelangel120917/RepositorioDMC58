4e93f47546e76bf82e3a6dd33a54d7656e3bad66
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-12 19:47:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Miguel Limaquispe")
+
+sesion = st.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
