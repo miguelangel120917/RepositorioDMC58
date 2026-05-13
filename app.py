004aaf2499eb656065cd29bfa2934f9a54d1358e
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-12 19:58:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2"
 
 if sesion == "Sesión 1":
   st.write("Bienvenido a la sesión 1")
+  st.image("Python_logo.png")
 elif sesion =="Sesión 2":
   st.write("Bienvenido a la sesión 2")
 elif sesion =="Sesión 3":
