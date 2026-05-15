74d7cede2486022094d464731306fd234eb35964
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-14 21:10:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.title("Mi primera aplicación en python")
 
@@ -32,7 +33,8 @@ elif sesion == "Sesión 2":
 elif sesion == "Sesión 3":
 
   st.write("Bienvenido la sesión 3")
-  inicio_rango = st.slider("Seleccione un valor", min_value=0,max_value=20,value=7)
+  fin_rango = st.slider("Seleccione un valor", min_value=0,max_value=20,value=7)
+  arreglo = np.arange(0,fin_rango)
 
 else:
 
