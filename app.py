4b4c02efb422bca0fb6b886cdfae7823e3c2a502
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-14 21:38:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ elif sesion == "Sesión 3":
 elif sesion == "Sesión 4":
   st.write("Bienvenido la sesión 4")
   principal =  st.number_imput("Ingrese el monto del préstamo", value=1000)
-  tasa_anual =  st.number_imput("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0)
+  tasa_anual =  st.number_imput("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0, max_value=1.0)
   anios =  st.number_imput("Ingrese el años de préstamo", value=1)
-  pagos_x_anio =  st.number_imput("Ingrese la cantiodad de pagos por año", value=12)
+  pagos_anio =  st.number_imput("Ingrese la cantidad de pagos por año", value=12)
 
   cuota= lf.cuota_prestamo(principal,tasa_anual,anios,pagos_x_anio)
   st.write(f"El valor de la cuota es {cuota}")
